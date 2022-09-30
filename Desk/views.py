261c83c13dd1c593a6b10962c20abb394e99f860
Prafull <prafullpandey614@gmail.com>
2022-09-30 13:51:08 +0530

    About us Logic Added in View

--- Desk/views.py
+++ Desk/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.views import Views
+from django.views.generic import TemplateView
 # Create your views here.#csc
 def index(request):
     return render(request, 'Desk/index.html')
 def add_a_book(request):
     return render(request, 'Desk/add_a_book.html')
+
+
+def aboutus(request):
+    return render(request, 'Desk/about_us.html')
+    
